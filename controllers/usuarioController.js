f9d7ad2c1c1aafe6a8c74528681a7a3adb9ed1dd
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-08-19 17:33:53 -0500

    Creando un nuevo usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,36 @@
-exports.crearUsuario = (req, res) =>{
-    console.log(req.body);
+//Importar el modelo usuario
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+
+
+exports.crearUsuario = async (req, res) =>{
+
+    //Extraer email y password
+    const {email, password} = req.body;
+
+    
+    try{
+        //Revisar que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({email});
+
+        if(usuario){
+            return res.status(400).json({msg: "El usuario ya existe"});
+        }
+
+        //Crear el nuevo usuario
+        usuario = new Usuario(req.body);
+
+        //Hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
+
+        //Guardar usuario
+        await usuario.save();
+
+        //Mensaje
+        res.send('Usuario creado correctamente');
+    }catch(error){
+        console.log(error);
+        res.status(400).json({msg: "Hubo un error"});
+    }
 }
