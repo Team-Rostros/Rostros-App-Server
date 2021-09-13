cec488026391f514730f75d99bdadcac52fdd8b8
stiven4950 <omarrocha4950@gmail.com>
2021-09-13 12:53:30 -0500

    Agregado campo creador tipo  ObjectID en el modelo desaparecidos

--- models/Desaparecido.js
+++ models/Desaparecido.js
@@ -3,151 +3,156 @@ const mongoose = require('mongoose');
 const DesaparecidosSchema = mongoose.Schema({
     // Información personal
     nombre1: {
-        type:String,
-	    required:true,
-	    trim:true
+        type: String,
+        required: true,
+        trim: true
     },
     nombre2: {
-        type:String,
-        required:true,
-        trim:true
+        type: String,
+        required: true,
+        trim: true
     },
-    apellido1:{
-        type:String,
-	    required:true,
-	    trim:true
+    apellido1: {
+        type: String,
+        required: true,
+        trim: true
     },
-    apellido2:{
-        type:String,
-	    required:true,
-	    trim:true
+    apellido2: {
+        type: String,
+        required: true,
+        trim: true
     },
-    genero:{
-        type:String,
-	    required:true,
-	    trim:true
+    genero: {
+        type: String,
+        required: true,
+        trim: true
     },
-    fechaNacimiento:{
+    fechaNacimiento: {
         type: Date,
-        required:true,
-        trim:true
+        required: true,
+        trim: true
     },
 
     // Ultima vez visto
-    pais:{
-        type:String,
-        required:true,
-        trim:true
+    pais: {
+        type: String,
+        required: true,
+        trim: true
     },
-    departamento:{
-        type:String,
-        required:true,
-        trim:true
+    departamento: {
+        type: String,
+        required: true,
+        trim: true
     },
-    ciudad:{
-        type:String,
-        required:true,
-        trim:true
+    ciudad: {
+        type: String,
+        required: true,
+        trim: true
     },
-    zipcode:{
-        type:String,
-        required:true,
-        trim:true
+    zipcode: {
+        type: String,
+        required: true,
+        trim: true
     },
-    fechad:{
-        type:Date,
-        required:true,
-        trim:true
+    fechad: {
+        type: Date,
+        required: true,
+        trim: true
     },
 
     // Características morfológicas 
-    craneo:{
-        type:String,
-        required:true
+    craneo: {
+        type: String,
+        required: true
     },
-    rostro:{
-        type:String,
-        required:true
+    rostro: {
+        type: String,
+        required: true
     },
-    cuello:{
-        type:String,
-        required:true
+    cuello: {
+        type: String,
+        required: true
     },
-    ojos:{
-        type:String,
-        required:true
+    ojos: {
+        type: String,
+        required: true
     },
-    cuerpo:{
-        type:String,
-        required:true
+    cuerpo: {
+        type: String,
+        required: true
     },
-    labios:{
-        type:String,
-        required:true
+    labios: {
+        type: String,
+        required: true
     },
-    oreja:{
-        type:String,
-        required:true
+    oreja: {
+        type: String,
+        required: true
     },
-    nariz:{
-        type:String,
-        required:true
+    nariz: {
+        type: String,
+        required: true
     },
 
     // Caracrerísticas físicas 
-    piel:{
-        type:String,
-        required:true
+    piel: {
+        type: String,
+        required: true
     },
-    cpiel:{
-        type:String,
-        required:true
+    cpiel: {
+        type: String,
+        required: true
     },
-    pigm:{
-        type:String,
-        required:true
+    pigm: {
+        type: String,
+        required: true
     },
-    peso:{
-        type:String,
-        required:true,
-        trim:true
+    peso: {
+        type: String,
+        required: true,
+        trim: true
     },
-    cabello:{
-        type:String,
-        required:true
+    cabello: {
+        type: String,
+        required: true
     },
-    ccabello:{
-        type:String,
-        required:true
+    ccabello: {
+        type: String,
+        required: true
     },
-    cojos:{
-        type:String,
-        required:true
+    cojos: {
+        type: String,
+        required: true
     },
-    clabios:{
-        type:String,
-        required:true
+    clabios: {
+        type: String,
+        required: true
     },
-    estatura:{
-        type:String,
-        required:true
+    estatura: {
+        type: String,
+        required: true
     },
-    cdientes:{
-        type:String,
-        required:true
+    cdientes: {
+        type: String,
+        required: true
     },
-    descripcion:{
-        type:String,
-        required:true
+    descripcion: {
+        type: String,
+        required: true
     },
-    registro:{
-        type:Date,
-        default:Date.now()
+    registro: {
+        type: Date,
+        default: Date.now()
     },
     photo: {
         data: Buffer,
         contentType: String,
-        required:false
+        required: false
+    },
+    creador: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario',
+        require:true,
     }
 });
 
